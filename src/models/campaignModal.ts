9033fbaa63c712f0953536baa53edea3f356eaaf
EmekaManuel <marrnuel123@gmail.com>
2024-11-14 15:03:37 +0100

    chore(): fixed model naming to correlate with the property name on the frontend

--- src/models/campaignModal.ts
+++ src/models/campaignModal.ts
@@ -5,52 +5,54 @@ interface CampaignData {
   email: string;
   title: string;
   name: string;
-  admissionProofUrl: string;
-  universityName: string;
-  matricNumber: string;
-  courseOfStudy: string;
-  yearOfEntry: number;
-  studentImageUrl: string;
-  studentResultImageUrl: string;
-  fundingReason: string;
-  projectLink: string;
+  admission_proof_url: string;
+  university_name: string;
+  matric_number: string;
+  course_of_study: string;
+  year_of_entry: number;
+  student_image_url: string;
+  student_result_image_url: string;
+  funding_reason: string;
+  project_link: string;
   goal: number;
-  startAt: Date;
-  endAt: Date;
+  start_at: Date;
+  end_at: Date;
   status: "pending" | "approved" | "rejected";
-  createdAt: Date;
-  updatedAt?: Date;
+  created_at: Date;
+  updated_at?: Date;
   authorized: boolean;
 }
 
-const campaignSchema: Schema<CampaignData> = new mongoose.Schema({
-  email: { type: String, required: true, match: /.+\@.+\..+/ },
-  title: { type: String, required: true, minlength: 2 },
-  name: { type: String, required: true, minlength: 2 },
-  admissionProofUrl: { type: String, required: true },
-  universityName: { type: String, required: true, minlength: 2 },
-  matricNumber: { type: String, required: true, minlength: 2 },
-  courseOfStudy: { type: String, required: true, minlength: 2 },
-  yearOfEntry: { type: Number, required: true, min: 1900 },
-  studentImageUrl: { type: String, required: true },
-  studentResultImageUrl: { type: String, required: true },
-  fundingReason: { type: String, required: true, minlength: 10 },
-  projectLink: {
-    type: String,
-    required: true,
-    match: /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i,
+const campaignSchema: Schema<CampaignData> = new mongoose.Schema(
+  {
+    email: { type: String, required: true, match: /.+\@.+\..+/ },
+    title: { type: String, required: true, minlength: 2 },
+    name: { type: String, required: true, minlength: 2 },
+    admission_proof_url: { type: String, required: true },
+    university_name: { type: String, required: true, minlength: 2 },
+    matric_number: { type: String, required: true, minlength: 2 },
+    course_of_study: { type: String, required: true, minlength: 2 },
+    year_of_entry: { type: Number, required: true, min: 1900 },
+    student_image_url: { type: String, required: true },
+    student_result_image_url: { type: String, required: true },
+    funding_reason: { type: String, required: true, minlength: 10 },
+    project_link: {
+      type: String,
+      required: true,
+      match: /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i,
+    },
+    goal: { type: Number, required: true, min: 1 },
+    start_at: { type: Date, required: true },
+    end_at: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+    authorized: { type: Boolean, default: false },
   },
-  goal: { type: Number, required: true, min: 1 },
-  startAt: { type: Date, required: true },
-  endAt: { type: Date, required: true },
-  status: {
-    type: String,
-    enum: ["pending", "approved", "rejected"],
-    default: "pending",
-  },
-
-  authorized: { type: Boolean, default: false },
-});
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
 
 // Export the model
 const CampaignModel = mongoose.model<CampaignData>("Campaign", campaignSchema);
